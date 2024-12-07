c6f162f8aaa0b9f1bb878e8abec933e7ed4778b9
dichenafederico <dichea..federico@gmail.com>
2024-12-07 20:04:31 -0300

    agrego config para github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 module.exports = {
     swcMinify: false,
+    output: 'export',
+    basePath: '/ArmonicaWeb-Next',
+    assetPrefix: '/ArmonicaWeb-Next/',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
       },
